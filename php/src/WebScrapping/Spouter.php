2247cbfa282a126b4df1e608f0f504786f60bb7c
IurydJR <iury.djr.ti@gmail.com>
2024-05-14 23:16:56 -0300

    feat(Spouter): implement new class Spouter that writes xlsx documents

---
+++ php/src/WebScrapping/Spouter.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Chuva\Php\WebScrapping;
+
+require_once '../../../vendor/autoload.php';
+
+class Spouter {
+
+    public function spouter ($papers) {
+
+        $writer = WriterEntityFactory::createXLSXWriter();
+        $writer->openToFile(__DIR__ .'/../../assets/papers_'. date("d-m-Y") .'.xlsx'); 
+
+        foreach ($papers as $article) {
+            $cells = [];
+            $cells = [
+                WriterEntityFactory::createCell($article->id),
+                WriterEntityFactory::createCell($article->title),
+                WriterEntityFactory::createCell($article->type),
+            ];
+            $authors = $article->authors;
+            $numAuthor = count($authors);;
+            foreach ($authors as $author) {
+                $cells[]= WriterEntityFactory::createCell($author->name);
+                $cells[]= WriterEntityFactory::createCell($author->institution);
+            }
+            
+            $singleRow = WriterEntityFactory::createRow($cells);
+            $writer->addRow($singleRow);
+        }
+    }
+}
